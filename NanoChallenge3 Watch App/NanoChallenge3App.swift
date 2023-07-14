8dd68a48269f6d124113f407dfcd2b2b27baf03e
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2023-07-14 14:32:43 +0700

    Initial Commit

---
+++ NanoChallenge3 Watch App/NanoChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge3App.swift
+//  NanoChallenge3 Watch App
+//
+//  Created by Nyoman Adiwinanda on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NanoChallenge3/NanoChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge3App.swift
+//  NanoChallenge3
+//
+//  Created by Nyoman Adiwinanda on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
