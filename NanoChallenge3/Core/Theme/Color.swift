0374d30b5f96aa7be3ae8e256ec7dabd43811930
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2023-07-22 01:38:29 +0700

    fix conflict

--- NanoChallenge3/Core/Theme/Color.swift
+++ NanoChallenge3/Core/Theme/Color.swift
@@ -14,5 +14,9 @@ extension UIColor{
     static var green_base: UIColor{ return UIColor(red: 52/255, green: 181/255, blue: 89/255, alpha: 1)}
     static var gray_base: UIColor{ return UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)}
     static var gray2_base: UIColor{ return UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1)}
+    static var gray3_base: UIColor{ return UIColor(red: 253/255, green: 253/255, blue: 253/255, alpha: 1)}
+    static var gray4_base: UIColor{ return UIColor(red: 166/255, green: 166/255, blue: 166/255, alpha: 1)}
+    static var outline_gray: UIColor{ return UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)}
+    static var destructive_red: UIColor{ return UIColor(red: 255/255, green: 59/255, blue: 47/255, alpha: 1)}
     static var pickerTextColor: UIColor{ return UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)}
 }
